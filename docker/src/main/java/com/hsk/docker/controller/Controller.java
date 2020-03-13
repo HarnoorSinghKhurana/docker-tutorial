9c9d0b3cf9121ab73e2e6acc452714ea594ff48d
Harnoor Singh <harnoorsingh@Harnoor-Singh-MacBook-Pro.local>
2020-03-13 23:55:41 +0530

    REST project with spotify plugin added and react frontend

---
+++ docker/src/main/java/com/hsk/docker/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.hsk.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping
+    public String getResponse(){
+        return "Welcome";
+    }
+}
